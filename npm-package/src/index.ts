866c909b1510464d437b55fcfeb3fddd09a6ba8f
Sergey Bondar <abobopw@gmail.com>
2025-11-23 16:02:22 +0100

    feat: feat

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/b'
 
 const lol = 'kek'
-const string = 'aфaddaddвфdsdsыadasвфыdasвыфdвфывыфвffgdfgdasf';
+const string = 'das';
 
 console.log('!I am an npm package', lol, string)
 
